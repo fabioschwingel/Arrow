c3219a86cb5f93d03f06e4f18896b7442f4955de
Fabio Cesar <fabio_schwingel@hotmail.com>
2018-09-02 13:51:30 +0000

    Revert "Remoção de linha"
    This reverts commit 2614fcca4a5d515b5fb8128135a94ed98fcbcbe2.

--- canario_negro.sh
+++ canario_negro.sh
@@ -3,4 +3,5 @@ echo "Script da Canario"
 cowsay -f tux $(fortune)
 
 ping -c2 192.168.99.20 >- && echo "Com conexão" || echo "Sem Conexão"
+ping -c2 192.168.99.99 >- && echo "Com conexão" || echo "Sem Conexão"
 
